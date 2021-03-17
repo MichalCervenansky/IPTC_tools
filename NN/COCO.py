799138c6abd9135d35ec62330ff1224aae6769f0
Michal Cervenansky <mcervena@redhat.com>
2021-03-17 16:43:38 +0100

    OpenImages and Coco dataset models added

--- NN/COCO.py
+++ NN/COCO.py
@@ -30,21 +30,6 @@ def display_image(image):
     plt.show()
 
 
-def download_and_resize_image(url, new_width=256, new_height=256,
-                              display=False):
-    _, filename = tempfile.mkstemp(suffix=".jpg")
-    response = urlopen(url)
-    image_data = response.read()
-    image_data = BytesIO(image_data)
-    pil_image = Image.open(image_data)
-    pil_image = ImageOps.fit(pil_image, (new_width, new_height), Image.ANTIALIAS)
-    pil_image_rgb = pil_image.convert("RGB")
-    pil_image_rgb.save(filename, format="JPEG", quality=90)
-    print("Image downloaded to %s." % filename)
-    if display:
-        display_image(pil_image)
-    return filename
-
 
 def draw_bounding_box_on_image(image,
                                ymin,
@@ -166,7 +151,7 @@ def run_detector(detector, path):
     display_image(image_with_boxes)
 
 
-img = load_img("img_524.jpg")
+img = load_img("img_99.jpg")
 image_tensor = tf.image.convert_image_dtype(img, tf.uint8)[tf.newaxis, ...]
 detector = hub.load("https://tfhub.dev/tensorflow/efficientdet/d7/1")
 
--- NN/openimages_resnet.py
+++ NN/openimages_resnet.py
@@ -5,9 +5,6 @@ import tensorflow_hub as hub
 
 # For downloading the image.
 import matplotlib.pyplot as plt
-import tempfile
-from six.moves.urllib.request import urlopen
-from six import BytesIO
 
 # For drawing onto the image.
 import numpy as np
@@ -15,10 +12,12 @@ from PIL import Image
 from PIL import ImageColor
 from PIL import ImageDraw
 from PIL import ImageFont
-from PIL import ImageOps
 
 # For measuring the inference time.
 import time
+
+from NN.Object_cropping import crop_out_boxes
+import pandas as pd
 
 
 def display_image(image):
@@ -26,22 +25,6 @@ def display_image(image):
     plt.grid(False)
     plt.imshow(image)
     plt.show()
-
-
-def download_and_resize_image(url, new_width=256, new_height=256,
-                              display=False):
-    _, filename = tempfile.mkstemp(suffix=".jpg")
-    response = urlopen(url)
-    image_data = response.read()
-    image_data = BytesIO(image_data)
-    pil_image = Image.open(image_data)
-    pil_image = ImageOps.fit(pil_image, (new_width, new_height), Image.ANTIALIAS)
-    pil_image_rgb = pil_image.convert("RGB")
-    pil_image_rgb.save(filename, format="JPEG", quality=90)
-    print("Image downloaded to %s." % filename)
-    if display:
-        display_image(pil_image)
-    return filename
 
 
 def draw_bounding_box_on_image(image,
@@ -88,7 +71,7 @@ def draw_bounding_box_on_image(image,
         text_bottom -= text_height - 2 * margin
 
 
-def draw_boxes(image, boxes, class_names, scores, max_boxes=10, min_score=0.1):
+def draw_boxes(image, boxes, class_names, scores):
     """Overlay labeled boxes on an image with formatted scores and label names."""
     colors = list(ImageColor.colormap.values())
 
@@ -99,25 +82,22 @@ def draw_boxes(image, boxes, class_names, scores, max_boxes=10, min_score=0.1):
         print("Font not found, using default font.")
         font = ImageFont.load_default()
 
-    for i in range(min(boxes.shape[0], max_boxes)):
-        print(scores[0])
-        if scores[i] >= min_score:
-            print(tuple(boxes[i]))
-            ymin, xmin, ymax, xmax = tuple(boxes[i])
-            display_str = "{}: {}%".format(class_names[i].decode("ascii"),
-                                           int(100 * scores[i]))
-            color = colors[hash(class_names[i]) % len(colors)]
-            image_pil = Image.fromarray(np.uint8(image)).convert("RGB")
-            draw_bounding_box_on_image(
-                image_pil,
-                ymin,
-                xmin,
-                ymax,
-                xmax,
-                color,
-                font,
-                display_str_list=[display_str])
-            np.copyto(image, np.array(image_pil))
+    for i in range(len(boxes)):
+        ymin, xmin, ymax, xmax = tuple(boxes[i])
+        display_str = "{}: {}%".format(class_names[i].decode("ascii"),
+                                       int(100 * scores[i]))
+        color = colors[hash(class_names[i]) % len(colors)]
+        image_pil = Image.fromarray(np.uint8(image)).convert("RGB")
+        draw_bounding_box_on_image(
+            image_pil,
+            ymin,
+            xmin,
+            ymax,
+            xmax,
+            color,
+            font,
+            display_str_list=[display_str])
+        np.copyto(image, np.array(image_pil))
     return image
 
 
@@ -125,6 +105,26 @@ def load_img(path):
     img = tf.io.read_file(path)
     img = tf.image.decode_jpeg(img, channels=3)
     return img
+
+
+def filter_result(result, min_score, max_boxes):
+    accepted_indexes = []
+    num = 0
+    for index in range(len(result["detection_scores"])):
+        if result["detection_scores"][index] > min_score and num < max_boxes:
+            accepted_indexes.append(index)
+            num += 1
+    return accepted_indexes
+
+
+def create_filtered_dic(accepted_indexes, orig_result):
+    filtered_dic = {"detection_scores": [], "detection_boxes": [], "detection_class_entities": []}
+
+    for index in accepted_indexes:
+        filtered_dic["detection_scores"].append(orig_result["detection_scores"][index])
+        filtered_dic["detection_boxes"].append(orig_result["detection_boxes"][index])
+        filtered_dic["detection_class_entities"].append(orig_result["detection_class_entities"][index])
+    return filtered_dic
 
 
 def run_detector(detector, path):
@@ -136,20 +136,21 @@ def run_detector(detector, path):
     end_time = time.time()
 
     result = {key: value.numpy() for key, value in result.items()}
+    filtered_dic = create_filtered_dic(filter_result(result, 0.3, 10), result)
 
     print("Found %d objects." % len(result["detection_scores"]))
     print("Inference time: ", end_time - start_time)
 
     image_with_boxes = draw_boxes(
-        img.numpy(), result["detection_boxes"],
-        result["detection_class_entities"], result["detection_scores"])
+        img.numpy(), filtered_dic["detection_boxes"],
+        filtered_dic["detection_class_entities"], filtered_dic["detection_scores"])
 
     display_image(image_with_boxes)
+
+    crop_out_boxes(img.numpy(), filtered_dic["detection_boxes"])
 
 
 module_handle = "https://tfhub.dev/google/faster_rcnn/openimages_v4/inception_resnet_v2/1"
 
 detector = hub.load(module_handle).signatures['default']
-print(detector.inputs)
-
 run_detector(detector, "img_19.jpg")
